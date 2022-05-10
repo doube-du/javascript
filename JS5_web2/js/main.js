6fcb7489e9666aa5da3c6eea9b94b0ba5b7edf5c
Eduardo <eduardo.furlaneti@ecoassist.com.br>
2022-05-10 09:00:20 -0300

    Prosseguindo...

--- JS5_web2/js/main.js
+++ JS5_web2/js/main.js
@@ -1,3 +1,14 @@
-//DOM - Document Object Model [Conteúdo html]
+//DOM - Document Object Model [Conteúdo HTML do Documento]
 let h1 = document.querySelector('.titulo');
-h1.textContent = 'Treinando Javascript';
+// h1.textContent = 'Treinando Javascript';
+h1.textContent = 'Aparecida Nutricionista';
+
+/* Lendo Conteúdo */
+let pacientes = document.querySelectorAll('.paciente');
+pacientes.forEach(paciente => {
+    const nome      = paciente.querySelector('.info-nome').textContent;
+    const peso      = paciente.querySelector('.info-peso').textContent;
+    const altura    = paciente.querySelector('.info-altura').textContent;
+
+    console.log(`O IMC de ${nome} é o peso: ${peso} * ${altura}`)
+});
